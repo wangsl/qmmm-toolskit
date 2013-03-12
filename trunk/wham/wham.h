2a925f863d7113061b16d3e60b6d9e1d5d0f2cbd
wangsl2001@gmail.com <wangsl2001@gmail.com@80932def-f4cb-a9f4-c62c-8f05423abd25>
2013-03-12 20:09:07 +0000

    change wham.c to whan.C

--- trunk/wham/wham.h
+++ trunk/wham/wham.h
@@ -1,13 +1,26 @@
 
+
+/* $Id$ */
+
 #ifndef WHAM_H
 #define WHAM_H
 
 #include "fort.h"
 #include "fint.h"
 
-void FORT(wham_iteration_with_fortran)(double *ebw, FInt *Nwind, FInt *Niter, 
-				       double *tolerance, FInt *nt, FInt *maxnt, 
-				       double *kBT, double *ebf, double *ebf2,
-				       char *outtput, FInt *lenoutput);
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
+  void FORT(wham_iteration_with_fortran)(double *ebw, FInt *Nwind, FInt *Niter, 
+					 double *tolerance, FInt *nt, FInt *maxnt, 
+					 double *kBT, double *ebf, double *ebf2,
+					 char *outtput, FInt *lenoutput);
+
+  void initwhamit();
+  
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* WHAM_H */
